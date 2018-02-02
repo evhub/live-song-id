402de2e6936a6d01062647008d63f5e32971675b
Haunter17 <haunterhacker17@gmail.com>
2018-02-02 03:11:52 -0800

    preprocessing & notebook setup

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,13 @@
 import librosa
 import numpy as np
 
-def get_allpaths(artist, listdir):
+def get_allpaths(artist, listdir, file_type='ref'):
     # Get names of all wav files
     file_paths = []
     f = open(listdir+artist+"_reftoname.txt", 'r')
     for line in f:
         filename = (line.split('_'))[0]
-        file_paths.append(artist+'_'+line.split('_')[0])
+        file_paths.append(artist+'_'+file_type+line.split('_')[0])
     f.close()
     return file_paths
 
@@ -23,24 +23,24 @@ def preprocess(Q, ds):
 # audiopath - where audio files are stored
 # listdir - where list of softlinks are stored
 # cqtdir - where results will be stored
-audiopath = "/home/nbanerjee/SoftLinks/"
-listdir = "/home/nbanerjee/SoftLinks/Lists/"
-cqtdir = "/data1/mint/test"
-artist = "taylorswift"
-file_paths = get_allpaths(artist, listdir)
+# audiopath = "/home/nbanerjee/SoftLinks/"
+# listdir = "/home/nbanerjee/SoftLinks/Lists/"
+# cqtdir = "/data1/mint/test"
+# artist = "taylorswift"
+# file_paths = get_allpaths(artist, listdir)
 
-# Open file that will contain the paths to all preprocessed CQT files
-f = open(cqtdir+artist+"_cqtList.txt", 'w')
+# # Open file that will contain the paths to all preprocessed CQT files
+# f = open(cqtdir+artist+"_cqtList.txt", 'w')
 
-for curFile in file_paths:
-    y, sr = librosa.load(audiopath + curFile + '.wav')
-    Q = librosa.cqt(y, sr=sr, fmin = 130.81, n_bins = 121, bins_per_octave = 24)
-    logQ = preprocess(Q, 3)
+# for curFile in file_paths:
+#     y, sr = librosa.load(audiopath + curFile + '.wav')
+#     Q = librosa.cqt(y, sr=sr, fmin = 130.81, n_bins = 121, bins_per_octave = 24)
+#     logQ = preprocess(Q, 3)
     
-    # Save array
-    np.savetxt(cqtdir+curFile+'.dat', logQ)
+#     # Save array
+#     np.savetxt(cqtdir+curFile+'.dat', logQ)
 
-    # Write .dat file path to cqtList file
-    f.write(cqtdir+curFile+'.dat\n')
+#     # Write .dat file path to cqtList file
+#     f.write(cqtdir+curFile+'.dat\n')
 
-f.close()
+# f.close()
