9c7700af75dd35af385b278cf79dbd0c4d85c47a
Haunter17 <haunterhacker17@gmail.com>
2018-02-08 20:59:40 -0800

    ident error on utils

--- utils.py
+++ utils.py
@@ -1,29 +1,30 @@
 import numpy as np
 
 def pitch_shift_CQT(M, shiftBins):
-	'''
-		pitchshift equivalent of Prof Tsai's code in matlab
+    '''
+        pitchshift equivalent of Prof Tsai's code in matlab
 
-		M: a 2-D CQT matrix
-		shiftBins: An integer that indicates the pitch to shift to
+        M: a 2-D CQT matrix
+        shiftBins: An integer that indicates the pitch to shift to
 
-		return: a pitchshifted matrix
-	'''
-	shifted = np.roll(M, shiftBins, axis=0)
-	if shiftBins > 0:
-		shifted[:shiftBins, :] = 0.
-	else:
-		shifted[shiftBins:, :] = 0.
-	return shifted
+        return: a pitchshifted matrix
+    '''
+    shifted = np.roll(M, shiftBins, axis=0)
+    if shiftBins > 0:
+        shifted[:shiftBins, :] = 0.
+    else:
+        shifted[shiftBins:, :] = 0.
+    return shifted
 
 def get_querytoref(artist, listdir):
-	'''
-		artist: a string representing the artist
-		listdir: a string representing the directory of the .list file
+    '''
+        artist: a string representing the artist
+        listdir: a string representing the directory of the .list file
 
-		returns an array of integer, where each entry corresponds to the
-			reference index
-	'''
+        returns an array of integer, where each entry corresponds to the
+            reference index
+    '''
+
     ref_idxs = []
     f = open(listdir+artist+'_querytoref.list', 'r')
     for line in f:
