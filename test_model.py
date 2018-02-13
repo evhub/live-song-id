9b85e49e794e5bcd569d97abd490da3697e70e3b
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-13 12:47:47 -0800

    Fix import library

--- test_model.py
+++ test_model.py
@@ -4,17 +4,20 @@ Code for testing model.py
 
 import numpy as np
 import scipy.io
-
+from utils import *
+from model import *
 # Load eigenvectors
 mat = scipy.io.loadmat('/data1/mint/public/taylorswift_out/model.mat', squeeze_me=True)
 evecs = mat['eigvecs'][::-1][:64]
 assert evecs.shape == (64, 2420)
+print("evecs.shape == (64, 2420")
 
 # Load preprocessed CQT
 mat = scipy.io.loadmat('/data1/mint/public/taylorswift_out/taylorswift_preprocessed_ref1.mat', squeeze_me=True)
 Q = np.abs(mat['B']).T
 
 # Generate PCA matrix
+m = 20
 pca_matrix = np.array([vec.reshape((m, -1)) for vec in evecs])
 delta = 16
 max_pitch_shift = 4
@@ -36,3 +39,4 @@ mat = scipy.io.loadmat('/data1/mint/public/taylorswift_out/db.mat', squeeze_me=T
 expected_fpseq = np.array([ mat["beforeDeltas"][0][:,:,k] for k in range(9)])
 
 assert expected_fpseq.shape == fpseqs.shape
+print("expected_fpseq.shape == fpseqs.shape")
