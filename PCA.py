89430ea58f407a0d697a44a8c750226481ace1a4
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-14 16:28:08 -0800

    Fix PCA on notebook

--- PCA.py
+++ PCA.py
@@ -1,10 +1,12 @@
 import numpy as np
-from scipy import linalg as LA
 
 def getTDE(Q, m=20, tao=1, hop=5, nbins=121):
-    A = np.zeros((1, nbins*m//tao))
-    QT = np.transpose(Q)
-    for i in range(0,Q.shape[1]-(m*tao),hop):
-        temp = np.reshape(QT[i:i+m:tao,:], (1, -1))
-        A = np.append(A, np.reshape(QT[i:i+m:tao,:], (1, -1)), axis = 0)
-    return A[1:]
+    numFrames = Q.shape[1]
+    numPitches = Q.shape[0]
+    tdespan = tao * (m - 1) + 1
+    endIndex = numFrames - tdespan + 1
+    A = np.zeros((len(offsets), numPitches * m))
+    for i in range(m):
+        frameIdx = offsets + i * tao
+        A[:, i*numPitches:(i+1)*numPitches] = Q[:,frameIdx].T
+    return A
